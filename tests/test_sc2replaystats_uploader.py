ba55f98986a425d9d1a2d875347c74aff54df7a7
Dominik Stańczak <stanczakdominik@gmail.com>
2020-08-25 08:47:18 +0200

    'fix' tests

--- tests/test_sc2replaystats_uploader.py
+++ tests/test_sc2replaystats_uploader.py
@@ -3,31 +3,15 @@
 """Tests for `sc2replaystats_uploader` package."""
 
 
-import unittest
 from click.testing import CliRunner
 
 from sc2replaystats_uploader import sc2replaystats_uploader
 from sc2replaystats_uploader import cli
 
 
-class TestSc2replaystats_uploader(unittest.TestCase):
-    """Tests for `sc2replaystats_uploader` package."""
-
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
-
-    def test_command_line_interface(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'sc2replaystats_uploader.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+def test_command_line_interface():
+    """Test the CLI."""
+    runner = CliRunner()
+    help_result = runner.invoke(cli.main, ["--help"])
+    assert help_result.exit_code == 0
+    assert "Show this message and exit." in help_result.output
